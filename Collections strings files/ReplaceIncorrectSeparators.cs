be6a69f83712439165b4bdf666c1f13f5eaf2d32
Salavat <45713278+13cyberpunk02@users.noreply.github.com>
2022-08-08 06:44:02 +0300

    Create ReplaceIncorrectSeparators.cs

---
+++ Collections strings files/ReplaceIncorrectSeparators.cs
@@ -0,0 +1,9 @@
+public static string ReplaceIncorrectSeparators(string text)
+{
+    return text.Replace(" ", "\t").
+		Replace(",", "").
+		Replace(";", "").
+		Replace("-", "").
+		Replace(":", "").
+		Replace("\t\t", "\t");
+}
